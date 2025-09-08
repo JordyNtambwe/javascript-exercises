628a43d0c5b58582382b31aa7aa27e843dacd518
JordyNtambwe <jordyntambwe@gmail.com>
2025-09-07 18:32:23 -0600

    a function that takes 2 positive integers and returns the sum of every integer

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(sum1, sum2) {
 
+  let result = 0;
+
+  for (let i = 0; i < sum1; i++) {
+    let num = sum1 + sum2
+    if (Number.isInteger(num)) {
+      result += num
+    }
+  }
+  return result;
 };
+
+console.log(sumAll(1, 4));
 
 // Do not edit below this line
 module.exports = sumAll;
