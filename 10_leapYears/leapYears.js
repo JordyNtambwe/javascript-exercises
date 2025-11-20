1bf352e955860586bafd120f8cde5a8836ddb08c
JordyNtambwe <jordyntambwe@gmail.com>
2025-11-19 19:26:25 -0700

    created a function that determines whether or not a given year is a leap year

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const leapYearsDivisibleByFour = year % 4 === 0;
+  const aCentury = year % 100 === 0;
+  const leapYearsDivisibleByFourHundred = year % 400 === 0;
 
+  if (
+    leapYearsDivisibleByFour &&
+    (!aCentury || leapYearsDivisibleByFourHundred)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+console.log(leapYears(2000));
+console.log(leapYears(1985));
 
 // Do not edit below this line
 module.exports = leapYears;
