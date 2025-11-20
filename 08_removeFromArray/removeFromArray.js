d13d505b0024e7c7f103b759cc631dc0f391a83b
JordyNtambwe <jordyntambwe@gmail.com>
2025-11-19 18:53:13 -0700

    created  a function that takes an array and some other arguments then removes the other arguments from that array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...elementToRemove) {
+  return array.filter(item => !elementToRemove.includes(item));
 };
-
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
