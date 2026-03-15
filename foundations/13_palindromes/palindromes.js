67bcfe1bdcf64fd30a2fc13c9b97694530fd245b
JordyNtambwe <jordyntambwe@gmail.com>
2026-03-15 12:51:56 -0600

    created a function that determines whether is a string is palindrome or not

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const alphabet = abcdefghijklmnopqrstuvwxyz;
+  let notReversed = string
+  .toLowerCase()
+  .split('')
+  .filter((character) => alphabet.includes(character))
+  .join('')
+  let reversed = notReversed.split('').reverse().join('')
+  if (notReversed === reversed) {
+    return true;
+  } else {
+    return false;
+  };
 };
 
+console.log(palindromes('racecar'));
+console.log(palindromes('tacos'));
 // Do not edit below this line
 module.exports = palindromes;
