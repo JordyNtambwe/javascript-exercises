ab3c867c369596140348ab7c825760a002b2fa09
JordyNtambwe <jordyntambwe@gmail.com>
2026-03-15 13:14:30 -0600

    created a function that returns a specific member of the fibonacci square

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(count) {
+  if (count < 0) {
+    return;
+  }
+  if (count === 0) {
+    return 0;
+  };
+  let a = 0;
+  let b = 1;
+  for (let i = 0; i < count; i++) {
+    const temp = b;
+    b = a + b;
+    a = temp;
+  };
+  return temp;
 };
+
+console.log(fibonacci(4));
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
